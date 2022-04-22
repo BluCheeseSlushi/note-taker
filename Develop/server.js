f0be846ef18a1507fd39973eef7db95d9766e4b7
Josiah Douglass <josiahdouglass64@gmail.com>
2022-04-22 13:28:35 -0400

    acceptance criteria met

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,31 @@
 const express = require('express');
-const notes = require('./db/db.json');
-// const html = require('./public/notes.html');
+const { notes } = require('./db/db.json');
+const fs = require('fs');
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(express.static(__dirname + '/public'));
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
-// app.get('/assets/js/index.js', (req, res) => {
-//     res.sendfile('./public/assets/js/index.js');
-//     res.sendfile('./public/assets/css/styles.css');
-// })
+function createNewNote(body, notes) {
+    const newNote = body;
+    notes.push(newNote);
+    fs.writeFileSync(
+      path.join(__dirname, "./db/db.json"),
+      JSON.stringify({ notes }, null, 2)
+    );
+    return newNote;
+}
 
 app.post('/api/notes', (req,res) =>{
-    console.log(req.body);
-    res.json(req.body);
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+
+    const postedNote = createNewNote(req.body, notes);
+    res.json(postedNote);
 })
 
 app.get('/notes', (req, res) =>{
